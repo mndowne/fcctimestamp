74da44eab537bdcd98812098c02d5e8d9df03a96
Marty <mndowne@gmail.com>
2016-10-22 12:45:22 +0000

    port change to deploy on heroku

--- express.js
+++ express.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 
+app.set('port', (process.env.PORT || 8080));
 /*app.get('/',function(req,res){
   res.sendFile(__dirname + '/index.html');
 });*/
@@ -122,4 +123,4 @@ app.get('/:date', function(req,res){
   res.json(json);
 });
 
-app.listen(8080, function () {console.log('server is on 8080');});
+app.listen(app.get('port'), function () {console.log('server is on 8080 or on the process enviornment port');});
