cea64a25bdeff201a188aa561544a18895dad577
Marty <mndowne@gmail.com>
2016-10-22 05:36:55 +0000

    The api is all done, maybe ill change index.html later

--- express.js
+++ express.js
@@ -8,8 +8,109 @@ var app = express();
 app.use(express.static(__dirname));
 
 app.get('/:date', function(req,res){
-  console.log(req.params.month);  
-  res.send(req.params.month);
+  
+  var arr = req.params.date.split(' ');
+  
+  
+  if (isNaN(req.params.date) == false){
+  var nDate = new Date(req.params.date*1000);
+  var nDateMonth = nDate.getMonth() +1;
+  var nDateYear = nDate.getFullYear();
+  var nDateDay = nDate.getDate();
+  var stamp = req.params.date;
+  switch (nDateMonth){
+    case 1:
+      nDateMonth = "January";
+      break;
+    case 2:
+      nDateMonth = "Febuary";
+      break;
+    case 3:
+      nDateMonth = "March";
+      break;
+    case 4:
+      nDateMonth = "April";
+      break;
+    case 5:
+      nDateMonth = "May";
+      break;
+    case 6:
+      nDateMonth = "June";
+      break;
+    case 7:
+      nDateMonth = "July";
+      break;
+    case 8:
+      nDateMonth = "August";
+      break;
+    case 9:
+      nDateMonth = "September";
+      break;
+    case 10:
+      nDateMonth = "October";
+      break;
+    case 11:
+      nDateMonth = "November";
+      break;
+    case 12:
+      nDateMonth = "December";
+      break;
+   }
+   
+  var nDateStr = nDateMonth + " " + nDateDay + ", " + nDateYear;
+
+  var json = {unix: stamp,
+              natural: nDateStr }; 
+  }
+  else if (arr.length == 3){
+    var month = -1; 
+    if (arr[0] == "January"){
+      month = 0;
+    }
+    else if (arr[0] == "Febuary"){
+      month = 1;
+    }
+    else if (arr[0] == "March"){
+      month = 2;
+    }
+    else if (arr[0] == "April"){
+      month = 3;
+    }
+    else if (arr[0] == "May"){
+      month = 4;
+    }
+    else if (arr[0] == "June"){
+      month = 5;
+    }
+    else if (arr[0] == "July"){
+      month = 6;
+    }
+    else if (arr[0] == "August"){
+      month = 7;
+    }
+    else if (arr[0] == "September"){
+      month = 8;
+    }
+    else if (arr[0] == "October"){
+      month = 9;
+    }
+    else if (arr[0] == "November"){
+      month = 10;
+    }
+    else if (arr[0] == "December"){
+      month = 11;
+    }
+    console.log('poop');
+  var d = new Date(arr[2],month,arr[1]); 
+ var stamp = Math.floor(d / 1000); 
+  var nDateStr = arr[0] + " " + arr[1] + ", " + arr[2];
+  
+  var json = {unix: stamp,
+              natural: nDateStr }; 
+  }
+
+  console.log(req.params.date);  
+  res.json(json);
 });
 
 app.listen(8080, function () {console.log('server is on 8080');});
